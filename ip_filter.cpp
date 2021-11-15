8d5d3d451ad818612a03c6b4142e591266c4daae
krilatiy <pos@list.ru>
2021-11-15 16:21:17 +0300

    replace 4 spaces with tab

--- ip_filter.cpp
+++ ip_filter.cpp
@@ -20,19 +20,19 @@ ip split(const std::string &str, char d)
 {
 	ip r;
 
-    std::string::size_type start = 0;
-    std::string::size_type stop = str.find_first_of(d);
-    while(stop != std::string::npos)
-    {
-        r.push_back(str.substr(start, stop - start));
+	std::string::size_type start = 0;
+	std::string::size_type stop = str.find_first_of(d);
+	while(stop != std::string::npos)
+	{
+		r.push_back(str.substr(start, stop - start));
 
-        start = stop + 1;
-        stop = str.find_first_of(d, start);
-    }
+		start = stop + 1;
+		stop = str.find_first_of(d, start);
+	}
 
-    r.push_back(str.substr(start));
+	r.push_back(str.substr(start));
 
-    return r;
+	return r;
 }
 
 void print_ip_pool(ip_vec& ip_pool)
@@ -89,21 +89,21 @@ int main(int argc, char const *argv[])
 {
 	for (int i = 1; i < argc; i++)
 		puts(argv[i]);
-    try
-    {
+	try
+	{
 		ip_vec ip_pool;
 		ip_pool.reserve(100);
-        for(std::string line; std::getline(std::cin, line);)
-        {
-            ip v = split(line, '\t');
+		for(std::string line; std::getline(std::cin, line);)
+		{
+			ip v = split(line, '\t');
 			v = split((v.at(0)), '.');
 			ip_int v_int;
 			for (auto ip_part : v)
 				v_int.push_back(std::stoi(ip_part));
-            ip_pool.push_back(v_int);
-        }
+			ip_pool.push_back(v_int);
+		}
 
-        // reverse lexicographically sort
+		// reverse lexicographically sort
 		
 		sort(ip_pool.begin(), ip_pool.end(), [](const ip_int& a, const ip_int& b) {
 			return ((unsigned)(((a[0] * 256 + a[1]) * 256 + a[2]) * 256 + a[3])) > 
@@ -112,79 +112,79 @@ int main(int argc, char const *argv[])
 
 		print_ip_pool(ip_pool);
 
-        // 222.173.235.246
-        // 222.130.177.64
-        // 222.82.198.61
-        // ...
-        // 1.70.44.170
-        // 1.29.168.152
-        // 1.1.234.8
+		// 222.173.235.246
+		// 222.130.177.64
+		// 222.82.198.61
+		// ...
+		// 1.70.44.170
+		// 1.29.168.152
+		// 1.1.234.8
 
-        // filter by first byte and output
+		// filter by first byte and output
 		ip_vec ip_pool_filtered = filter(ip_pool, 1);
 		print_ip_pool(ip_pool_filtered);
 		ip_pool_filtered.clear();
 		
-        // 1.231.69.33
-        // 1.87.203.225
-        // 1.70.44.170
-        // 1.29.168.152
-        // 1.1.234.8
+		// 1.231.69.33
+		// 1.87.203.225
+		// 1.70.44.170
+		// 1.29.168.152
+		// 1.1.234.8
 
-        // filter by first and second bytes and output
+		// filter by first and second bytes and output
 		ip_pool_filtered = filter(ip_pool, 46, 70);
 		print_ip_pool(ip_pool_filtered);
 		ip_pool_filtered.clear();		
 
-        // 46.70.225.39
-        // 46.70.147.26
-        // 46.70.113.73
-        // 46.70.29.76
+		// 46.70.225.39
+		// 46.70.147.26
+		// 46.70.113.73
+		// 46.70.29.76
 
-        // filter by any byte and output
+		// filter by any byte and output
 		ip_pool_filtered = filter_any(ip_pool, 46);
 		print_ip_pool(ip_pool_filtered);
 		ip_pool_filtered.clear();
 
-        // 186.204.34.46
-        // 186.46.222.194
-        // 185.46.87.231
-        // 185.46.86.132
-        // 185.46.86.131
-        // 185.46.86.131
-        // 185.46.86.22
-        // 185.46.85.204
-        // 185.46.85.78
-        // 68.46.218.208
-        // 46.251.197.23
-        // 46.223.254.56
-        // 46.223.254.56
-        // 46.182.19.219
-        // 46.161.63.66
-        // 46.161.61.51
-        // 46.161.60.92
-        // 46.161.60.35
-        // 46.161.58.202
-        // 46.161.56.241
-        // 46.161.56.203
-        // 46.161.56.174
-        // 46.161.56.106
-        // 46.161.56.106
-        // 46.101.163.119
-        // 46.101.127.145
-        // 46.70.225.39
-        // 46.70.147.26
-        // 46.70.113.73
-        // 46.70.29.76
-        // 46.55.46.98
-        // 46.49.43.85
-        // 39.46.86.85
-        // 5.189.203.46
-    }
-    catch(const std::exception &e)
-    {
-        std::cerr << e.what() <<  " error in line -" << __LINE__ << " " << "in file " << __FILE__ << std::endl;
-    }
+		// 186.204.34.46
+		// 186.46.222.194
+		// 185.46.87.231
+		// 185.46.86.132
+		// 185.46.86.131
+		// 185.46.86.131
+		// 185.46.86.22
+		// 185.46.85.204
+		// 185.46.85.78
+		// 68.46.218.208
+		// 46.251.197.23
+		// 46.223.254.56
+		// 46.223.254.56
+		// 46.182.19.219
+		// 46.161.63.66
+		// 46.161.61.51
+		// 46.161.60.92
+		// 46.161.60.35
+		// 46.161.58.202
+		// 46.161.56.241
+		// 46.161.56.203
+		// 46.161.56.174
+		// 46.161.56.106
+		// 46.161.56.106
+		// 46.101.163.119
+		// 46.101.127.145
+		// 46.70.225.39
+		// 46.70.147.26
+		// 46.70.113.73
+		// 46.70.29.76
+		// 46.55.46.98
+		// 46.49.43.85
+		// 39.46.86.85
+		// 5.189.203.46
+	}
+	catch(const std::exception &e)
+	{
+		std::cerr << e.what() <<  " error in line -" << __LINE__ << " " << "in file " << __FILE__ << std::endl;
+	}
 
-    return 0;
+	return 0;
 }
